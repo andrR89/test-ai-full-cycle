043e60abd62b80e1e717d5dac10ee0d4e42471fb
Andre Manoel Rogerio <mr.andre.rt0@gmail.com>
2026-03-19 19:31:03 -0300

    Revert "AI: Resolve issue #10"
    This reverts commit 8e09137a3c582b1dffa0ff754b1f815263acee8e.

--- backend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js'],
-  collectCoverageFrom: ['src/**/*.js'],
-  coverageThreshold: {
-    global: { lines: 80, functions: 80, branches: 80, statements: 80 },
-  },
-};
